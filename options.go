4c35d0256671169631b5c40655293ac41e54e27e
Josh Montoya <josh@montoya.io>
2021-02-25 15:45:58 -0800

    implement Options

---
+++ options.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+)
+
+type Options struct {
+	Key    string `json:"key"`
+	Secret string `json:"secret"`
+
+	Endpoint string `json:"endpoint"`
+	Region   string `json:"region"`
+
+	Bucket string `json:"bucket"`
+}
+
+func (o *Options) makeConfig() (cfg aws.Config) {
+	cfg.Credentials = credentials.NewStaticCredentials(o.Key, o.Secret, "")
+
+	if len(o.Endpoint) > 0 {
+		cfg.Endpoint = aws.String(o.Endpoint)
+	}
+
+	if len(o.Region) > 0 {
+		cfg.Region = aws.String(o.Region)
+	}
+
+	return
+}
