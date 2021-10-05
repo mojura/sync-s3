fc792eaad76c98e59fc9b09839d78385b13d5304
Josh Montoya <josh@montoya.io>
2021-10-05 14:03:04 -0700

    remove generateFilename

--- utils.go
+++ utils.go
@@ -1,8 +1,1 @@
 package s3
-
-import "fmt"
-
-// generateFilename will generate a Mojura filename from a given prefix and created at
-func generateFilename(prefix string, createdAt int64) string {
-	return fmt.Sprintf("%s%d.moj", prefix, createdAt)
-}
