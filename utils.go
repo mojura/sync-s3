cc78adb1043e5e4626138ce2e817a6bc925abf3e
Josh Montoya <josh@montoya.io>
2021-10-22 15:57:39 -0700

    implement helpers for error andling and input obj creation

--- utils.go
+++ utils.go
@@ -1,1 +1,39 @@
 package s3
+
+import (
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/hatchify/errors"
+)
+
+func getErrorFirstLine(err error) (msg string) {
+	spl := strings.SplitN(err.Error(), "\n", 2)
+	return spl[0]
+}
+
+func getErrorCode(msg string) (code string) {
+	spl := strings.SplitN(msg, ":", 2)
+	return spl[0]
+}
+
+func newGetInputObject(bucket, filename string) *s3.GetObjectInput {
+	var input s3.GetObjectInput
+	input.Bucket = aws.String(bucket)
+	input.Key = aws.String(filename)
+	return &input
+}
+
+func handleError(err error) error {
+	msg := getErrorFirstLine(err)
+	code := getErrorCode(msg)
+	switch code {
+	case s3.ErrCodeNoSuchKey:
+		return os.ErrNotExist
+
+	default:
+		return errors.New(msg)
+	}
+}
